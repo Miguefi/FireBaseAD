6f12f8d8ee279959b3116320dd851399b6079001
Stefy_Lissa <stefanialissa7@gmail.com>
2023-02-04 17:32:00 +0100

    Crear cuenta a sesión iniciada

--- app/src/main/java/com/example/firebasead/MainActivity.java
+++ app/src/main/java/com/example/firebasead/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.firebasead;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, Object> gestor = new HashMap<>();
+        Bundle bundle = this.getIntent().getExtras();
+
+        if(bundle != null) {
+            gestor = (Map<String, Object>) bundle.getSerializable("HashMap");
+            Toast toast = Toast.makeText(getApplicationContext(), "Sesión iniciada "+gestor.toString(), Toast.LENGTH_LONG);
+            toast.show();
+
+        }
+
     }
 }
--- app/src/main/java/com/example/firebasead/NuevoGestor.java
+++ app/src/main/java/com/example/firebasead/NuevoGestor.java
@@ -2,29 +2,34 @@ package com.example.firebasead;
 
 import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 public class NuevoGestor extends AppCompatActivity {
 
-    EditText nombre, apellido, contraseña, num_tel;
-    FloatingActionButton crearGestor;
+    EditText nombre, apellido, contraseña, num_tel, dni;
+    Button crearGestor;
     ImageView imagen;
 
     @Override
@@ -32,33 +37,43 @@ public class NuevoGestor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_gestor);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        imagen = findViewById(R.id.imagen);
+        imagen.setImageResource(R.drawable.ic_launcher_foreground);
+        dni = findViewById(R.id.dni);
         nombre = findViewById(R.id.nombre);
         apellido = findViewById(R.id.apellido);
         contraseña = findViewById(R.id.contraseña);
         num_tel = findViewById(R.id.telefono);
-        crearGestor = findViewById(R.id.añadirGestor);
+        crearGestor = findViewById(R.id.crear);
         crearGestor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Map<String, Object> gestor = new HashMap<>();
-                gestor.put("DNI", "5463350V");
+                gestor.put("DNI", dni.getText().toString());
                 gestor.put("Contraseña", contraseña.getText().toString());
                 gestor.put("Nombre", nombre.getText().toString());
                 gestor.put("Apellido", apellido.getText().toString());
                 gestor.put("Num_Tel", num_tel.getText().toString());
-                db.collection("Gestores").add(gestor)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                db.collection("Gestores").add(gestor).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
                         Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putSerializable("HashMap", (Serializable) gestor);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.w(TAG, "Error adding document", e);
+                        Toast toast = Toast.makeText(getApplicationContext(), "Error al crear cuenta", Toast.LENGTH_LONG);
+                        toast.show();
                     }
                 });
+
             }
         });
 
